9e46227b9abed1436d0785172a066d95580c7a10
aniket0880 <aabaravkar456@gmail.com>
2024-09-11 22:06:40 +0530

    FOL

--- src/components/Demo/About.jsx
+++ src/components/Demo/About.jsx
@@ -6,6 +6,7 @@ export const About = () => {
     width: '100px',
     height: '100px',
     objectFit: 'cover',
+    transition: 'transform 0.3s ease-in-out',
   };
 
   const centerContainerStyle = {
@@ -13,6 +14,7 @@ export const About = () => {
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
+    textAlign: 'center', 
   };
 
   const personContainerStyle = {
@@ -22,100 +24,129 @@ export const About = () => {
     margin: '25px', 
   };
 
+  const sectionHeadingStyle = {
+    fontSize: '24px',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: '15px', 
+    color: '#333', 
+  };
+
+  const handleHover = (event) => {
+    event.target.style.transform = 'scale(1.1)'; 
+  };
+
+  const handleHoverOut = (event) => {
+    event.target.style.transform = 'scale(1)'; 
+  };
+
   return (
     <div>
       <br></br>
-      <p>About US</p>
+      <p style={sectionHeadingStyle}>ABOUT US</p>
       <p>jfhqefkjqwvudjq fjcwcdfcawbdvjkawbvbwjbcdjfbcfejbefcjkewfbdefjcbewjkbcbdjkwbckjbewkcf
         cwehckwevkfcbfweukbfbcawdsckhuvb
       </p>
       <br></br>
       <br></br>
-      <p>Faculty Coordinator</p>
+      <p style={sectionHeadingStyle}>Faculty Coordinator</p>
       <div className="container" style={centerContainerStyle}>
         <div style={personContainerStyle}>
-          <img src="profile.jpg" alt="Person 1" style={{ ...circleImageStyle, width: '125px', height: '125px' }}/>
+          <img src="profile.jpg" alt="Person 1" style={{ ...circleImageStyle, width: '125px', height: '125px' }} onMouseOver={handleHover} 
+            onMouseOut={handleHoverOut}/>
           <p>Name 1</p>
           <p>Position 1</p>
         </div>
       </div>
       <br></br>
       <br></br>
-      <p>Presidence Council</p>
+      <p style={sectionHeadingStyle}>Presidence Council</p>
       <div className="container" style={centerContainerStyle}>
         <div style={personContainerStyle}>
-          <img src="profile.jpg" alt="Person 2" style={{ ...circleImageStyle, width: '115px', height: '115px' }} />
+          <img src="profile.jpg" alt="Person 2" style={{ ...circleImageStyle, width: '115px', height: '115px' }} onMouseOver={handleHover} 
+            onMouseOut={handleHoverOut} />
           <p>Name 2</p>
           <p>Position 2</p>
         </div>
 
         <div style={personContainerStyle}>
-          <img src="profile.jpg" alt="Person 3" style={{ ...circleImageStyle, width: '115px', height: '115px' }} />
+          <img src="profile.jpg" alt="Person 3" style={{ ...circleImageStyle, width: '115px', height: '115px' }} onMouseOver={handleHover} 
+            onMouseOut={handleHoverOut} />
           <p>Name 3</p>
           <p>Position 3</p>
         </div>
 
         <div style={personContainerStyle}>
-          <img src="profile.jpg" alt="Person 3" style={{ ...circleImageStyle, width: '115px', height: '115px' }} />
+          <img src="profile.jpg" alt="Person 3" style={{ ...circleImageStyle, width: '115px', height: '115px' }} onMouseOver={handleHover} 
+            onMouseOut={handleHoverOut} />
           <p>Name 3</p>
           <p>Position 3</p>
         </div>
 
         <div style={personContainerStyle}>
-          <img src="profile.jpg" alt="Person 3" style={{ ...circleImageStyle, width: '115px', height: '115px' }}/>
+          <img src="profile.jpg" alt="Person 3" style={{ ...circleImageStyle, width: '115px', height: '115px' }} onMouseOver={handleHover} 
+            onMouseOut={handleHoverOut}/>
           <p>Name 3</p>
           <p>Position 3</p>
         </div>
       </div>
       <br></br>
       <br></br>
-      <p>Department Leads</p>
+      <p style={sectionHeadingStyle}>Department Leads</p>
       <div className="container" style={centerContainerStyle}>
         <div style={personContainerStyle}>
-          <img src="profile.jpg" alt="Person 2" style={circleImageStyle} />
+          <img src="profile.jpg" alt="Person 2" style={circleImageStyle} onMouseOver={handleHover} 
+            onMouseOut={handleHoverOut} />
           <p>Name 2</p>
           <p>Position 2</p>
         </div>
 
         <div style={personContainerStyle}>
-          <img src="profile.jpg" alt="Person 3" style={circleImageStyle} />
+          <img src="profile.jpg" alt="Person 3" style={circleImageStyle} onMouseOver={handleHover} 
+            onMouseOut={handleHoverOut}/>
           <p>Name 3</p>
           <p>Position 3</p>
         </div>
 
         <div style={personContainerStyle}>
-          <img src="profile.jpg" alt="Person 3" style={circleImageStyle} />
+          <img src="profile.jpg" alt="Person 3" style={circleImageStyle} onMouseOver={handleHover} 
+            onMouseOut={handleHoverOut}/>
           <p>Name 3</p>
           <p>Position 3</p>
         </div>
 
         <div style={personContainerStyle}>
-          <img src="profile.jpg" alt="Person 3" style={circleImageStyle} />
+          <img src="profile.jpg" alt="Person 3" style={circleImageStyle} onMouseOver={handleHover} 
+            onMouseOut={handleHoverOut}/>
           <p>Name 3</p>
           <p>Position 3</p>
         </div>
       </div>
       <div className="container" style={centerContainerStyle}>
         <div style={personContainerStyle}>
-          <img src="profile.jpg" alt="Person 2" style={circleImageStyle} />
+          <img src="profile.jpg" alt="Person 2" style={circleImageStyle} onMouseOver={handleHover} 
+            onMouseOut={handleHoverOut}/>
           <p>Name 2</p>
           <p>Position 2</p>
         </div>
 
         <div style={personContainerStyle}>
-          <img src="profile.jpg" alt="Person 3" style={circleImageStyle} />
+          <img src="profile.jpg" alt="Person 3" style={circleImageStyle} onMouseOver={handleHover} 
+            onMouseOut={handleHoverOut}/>
           <p>Name 3</p>
           <p>Position 3</p>
         </div>
 
         <div style={personContainerStyle}>
-          <img src="profile.jpg" alt="Person 3" style={circleImageStyle} />
+          <img src="profile.jpg" alt="Person 3" style={circleImageStyle} onMouseOver={handleHover} 
+            onMouseOut={handleHoverOut}/>
           <p>Name 3</p>
           <p>Position 3</p>
         </div>
 
         <div style={personContainerStyle}>
-          <img src="profile.jpg" alt="Person 3" style={circleImageStyle} />
+          <img src="profile.jpg" alt="Person 3" style={circleImageStyle} onMouseOver={handleHover} 
+            onMouseOut={handleHoverOut}/>
           <p>Name 3</p>
           <p>Position 3</p>
         </div>
